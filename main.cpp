35e4a1318502bd17d7ff4df6699dc350cca9d9ce
voldien <voldiekami@gmail.com>
2021-07-09 09:11:28 +0200

    Added random generator and other minor changes.

--- main.cpp
+++ main.cpp
@@ -1,69 +1,88 @@
 #define CRCPP_USE_CPP11
 #define CRCPP_INCLUDE_ESOTERIC_CRC_DEFINITIONS
-#include<CRC.h>
+#include <CRC.h>
 
-#include <iomanip>  // Includes ::std::hex
+#include <cstdint> // Includes ::std::uint32_t
+#include <cstring>
+#include <iomanip>	// Includes ::std::hex
 #include <iostream> // Includes ::std::cout
-#include <cstdint>  // Includes ::std::uint32_t
-#include<vector>
-#include<cstring>
+#include <vector>
 
-void generateRandomMessage(std::vector<unsigned int>& data, int size){
-    for(int i = 0; i < size; i++){
-        data[i] = rand();
-    }
+// *Really* minimal PCG32 code / (c) 2014 M.E. O'Neill / pcg-random.org
+// Licensed under Apache License 2.0 (NO WARRANTY, etc. see website)
+
+typedef struct {
+	uint64_t state;
+	uint64_t inc;
+} pcg32_random_t;
+pcg32_random_t rng;
+
+uint32_t pcg32_random_r(pcg32_random_t *rng) {
+	uint64_t oldstate = rng->state;
+	// Advance internal state
+	rng->state = oldstate * 6364136223846793005ULL + (rng->inc | 1);
+	// Calculate output function (XSH RR), uses old state for max ILP
+	uint32_t xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
+	uint32_t rot = oldstate >> 59u;
+	return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
 }
 
-void addRandomNoise(const std::vector<unsigned int>& in, std::vector<unsigned int>& out){
-    for(int i = 0; i < in.size(); i++){
-        out[i] = in[i] -  rand() % 10 == 0 ? rand() : 0;
-    }
+void generateRandomMessage(std::vector<unsigned int> &data, int size) {
+	for (int i = 0; i < size; i++) {
+		data[i] = pcg32_random_r(&rng);
+	}
 }
 
-uint32_t compute32Xor(const std::vector<unsigned int>& data){
-    uint32_t checksum = data[0];
-        for(int i = 1; i < data.size(); i++){
-        checksum  ^= data[i];
-    }
-    return checksum;
+void addRandomNoise(const std::vector<unsigned int> &in, std::vector<unsigned int> &out) {
+	for (int i = 0; i < in.size(); i++) {
+		out[i] = in[i] ^ (unsigned int)pcg32_random_r(&rng) + (unsigned int)(pcg32_random_r(&rng) * 0.0001f);
+	}
 }
 
-uint32_t compute8Xor(const std::vector<unsigned int>& data){
-    uint8_t* p = (uint8_t*)data.data();
-    uint8_t checksum = p[0];
-    for(int i = 1; i < data.size() * 4; i++){
-        checksum ^= p[i];
-    }
-    return checksum & 0x7E;
+uint32_t compute32Xor(const std::vector<unsigned int> &data) {
+	uint32_t checksum = data[0];
+	for (int i = 1; i < data.size(); i++) {
+		checksum ^= data[i];
+	}
+	return checksum;
 }
 
-int main(int argc, const char** argv){
+uint32_t compute8Xor(const std::vector<unsigned int> &data) {
+	uint8_t *p = (uint8_t *)data.data();
+	uint8_t checksum = p[0];
+	for (int i = 1; i < data.size() * 4; i++) {
+		checksum ^= p[i];
+	}
+	return checksum;
+}
 
-    int samples = 5000000;
-    int nrCollision = 0;
+int main(int argc, const char **argv) {
 
-    std::vector<unsigned int> data(10);
-    std::vector<unsigned int> noise(10);
-    srand(time(NULL));
+	int samples = 10000000;
+	int nrCollision = 0;
 
-    for(int i = 0; i < samples; i++){
-        generateRandomMessage(data, 10);
-        addRandomNoise(data, noise);
+	std::vector<unsigned int> data(100);
+	std::vector<unsigned int> noise(100);
+	srand(time(NULL));
 
-        std::uint32_t originalCRC = CRC::Calculate(data.data(), data.size() * sizeof(unsigned int), CRC::CRC_8());
-        std::uint32_t noiseCRC = CRC::Calculate(noise.data(), noise.size()  * sizeof(unsigned int), CRC::CRC_8());	
+	for (int i = 0; i < samples; i++) {
+		generateRandomMessage(data, 100);
+		addRandomNoise(data, noise);
 
-    //    std::uint32_t originalCRC =  compute8Xor(data);
-    //     std::uint32_t noiseCRC = compute8Xor(noise);
+		std::uint32_t originalCRC = CRC::Calculate(data.data(), data.size() * sizeof(unsigned int), CRC::CRC_32());
+		std::uint32_t noiseCRC = CRC::Calculate(noise.data(), noise.size() * sizeof(unsigned int), CRC::CRC_32());
 
-        if(!std::strncmp((const char*)data.data(), (const char*)noise.data(), data.size() * 4) && originalCRC == noiseCRC){
+		// std::uint32_t originalCRC = compute8Xor(data);
+		// std::uint32_t noiseCRC = compute8Xor(noise);
 
-            nrCollision++;
-        }
+		if (std::strncmp((const char *)data.data(), (const char *)noise.data(), data.size() * 4) != 0 &&
+			originalCRC == noiseCRC) {
 
-	
-        // std::cout << std::hex << originalCRC << std::endl;
-        // std::cout << std::hex << noiseCRC << std::endl;
-    }
-    printf("CRC8 collision %d, %f!\n", nrCollision, (float)nrCollision / (float)samples);
+			nrCollision++;
+		}
+
+		// std::cout << std::hex << originalCRC << std::endl;
+		// std::cout << std::hex << noiseCRC << std::endl;
+	}
+	printf("CRC8 collision %d, %f!\n", nrCollision, (float)nrCollision / (float)samples);
 }

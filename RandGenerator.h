76f58b0de7d1ed9a3795ee0b70e746b0ddddd7ee
voldie <voldiekami@gmail.com>
2021-07-11 15:01:55 +0200

    Added random class impl

---
+++ RandGenerator.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "pcg_basic.h"
+#include <stdlib.h>
+#include <time.h>
+class RandGenerator {
+  public:
+	virtual uint32_t getRandom(void) = 0;
+};
+
+class PGSRandom : public RandGenerator {
+  public:
+	PGSRandom(void) {
+		srand(time(nullptr));
+		pcg32_srandom_r(&rng, rand(), rand());
+	}
+	virtual uint32_t getRandom(void) { return pcg32_random_r(&rng); }
+
+  private:
+	pcg32_random_t rng;
+};

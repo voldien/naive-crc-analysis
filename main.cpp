29fadaa8cc35e11ab7bff05225cb8d8a852ef152
voldie <voldiekami@gmail.com>
2021-07-12 16:54:02 +0200

    Improved logic and output text

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include <cxxopts.hpp>
 #include <iomanip>	// Includes ::std::hex
 #include <iostream> // Includes ::std::cout
+#include <stdexcept>
 #include <vector>
 
 void generateRandomMessage(std::vector<unsigned int> &data, unsigned int size, RandGenerator &gen) {
@@ -55,80 +56,97 @@ uint32_t compute8Xor(const std::vector<unsigned int> &data) {
 
 int main(int argc, const char **argv) {
 
-	uint64_t samples = 1000000000;
-	uint32_t dataSize = 5;
-	uint32_t nrChunk = 2000;
-	std::atomic_uint32_t nrCollision = 0;
-	std::atomic_uint64_t nrOfSamples = 0;
+	/*	*/
+	try {
+		uint64_t samples;
+		uint32_t dataSize;
+		uint32_t nrChunk = 2000;
+		std::atomic_uint32_t nrCollision = 0;
+		std::atomic_uint64_t nrOfSamples = 0;
 
-	uint32_t numTasks = marl::Thread::numLogicalCPUs() * nrChunk;
-	uint64_t localsamples = samples / numTasks;
+		cxxopts::Options options("Naive CRC Analysis", "");
+		options.add_options()("d,debug", "Enable debugging") // a bool parameter
+			("i,integer", "Int param", cxxopts::value<int>())("v,verbose", "Verbose output",
+															  cxxopts::value<bool>()->default_value("false"))(
+				"c,crc", "CRC", cxxopts::value<int>())("p,data-chunk-size", "DataChunk",
+													   cxxopts::value<int>()->default_value("5"))(
+				"e,error-correction", "Error Correction", cxxopts::value<bool>()->default_value("false"))(
+				"s,samples", "Samples", cxxopts::value<int>()->default_value("1000000"))
+				("t,task-size", "Task", cxxopts::value<int>()->default_value("2000"));
 
-	cxxopts::Options options("Naive CRC Analysis", "");
-	options.add_options()("d,debug", "Enable debugging") // a bool parameter
-		("i,integer", "Int param", cxxopts::value<int>())
-		("v,verbose", "Verbose output", cxxopts::value<bool>()->default_value("false"))
-		("c,crc", "CRC", cxxopts::value<int>())
-		("p,data-chunk", "DataChunk", cxxopts::value<int>())
-		("s,samples", "Samples", cxxopts::value<int>());
+		auto result = options.parse(argc, (char **&)argv);
 
-	auto result = options.parse(argc, (char **&)argv);
-	// result["opt"].as<float>();
+		dataSize = result["data-chunk-size"].as<int>();
+		samples = result["samples"].as<int>();
+		nrChunk = result["task-size"].as<int>();
 
-	marl::Scheduler scheduler(marl::Scheduler::Config::allCores());
-	scheduler.bind();
-	defer(scheduler.unbind()); // Automatically unbind before returning.
+		/*	*/
+		uint32_t numTasks = marl::Thread::numLogicalCPUs() * nrChunk;
+		uint64_t localsamples = samples / numTasks;
 
-	// Create an event that is manually reset.
-	marl::Event sayHello(marl::Event::Mode::Manual);
+		/*	*/
+		marl::Scheduler scheduler(marl::Scheduler::Config::allCores());
+		scheduler.bind();
+		defer(scheduler.unbind()); // Automatically unbind before returning.
 
-	// Create a WaitGroup with an initial count of numTasks.
-	marl::WaitGroup saidHello(numTasks);
+		// Create an event that is manually reset.
+		marl::Event sayHello(marl::Event::Mode::Manual);
+		// Create a WaitGroup with an initial count of numTasks.
+		marl::WaitGroup saidHello(numTasks);
 
-	for (uint32_t i = 0; i < numTasks; i++) {
-		marl::schedule([&] { // All marl primitives are capture-by-value.
-			std::vector<unsigned int> data(dataSize);
-			std::vector<unsigned int> noise(dataSize);
-			PGSRandom randGen;
+		for (uint32_t i = 0; i < numTasks; i++) {
+			marl::schedule([&] { // All marl primitives are capture-by-value.
+				std::vector<unsigned int> data(dataSize);
+				std::vector<unsigned int> noise(dataSize);
+				PGSRandom randGen;
 
-			for (uint64_t i = 0; i < localsamples; i++) {
-				generateRandomMessage(data, dataSize, randGen);
-				addRandomNoise(data, noise, randGen);
+				/*	*/
+				for (uint64_t i = 0; i < localsamples; i++) {
+					generateRandomMessage(data, dataSize, randGen);
+					addRandomNoise(data, noise, randGen);
 
-				// std::uint32_t originalCRC =
-				// 	CRC::Calculate(data.data(), data.size() * sizeof(unsigned int), CRC::CRC_5_USB());
-				// std::uint32_t noiseCRC =
-				// 	CRC::Calculate(noise.data(), noise.size() * sizeof(unsigned int), CRC::CRC_5_USB());
+					std::uint32_t originalCRC =
+						CRC::Calculate(data.data(), data.size() * sizeof(unsigned int), CRC::CRC_8());
+					std::uint32_t noiseCRC =
+						CRC::Calculate(noise.data(), noise.size() * sizeof(unsigned int), CRC::CRC_8());
 
-				std::uint32_t originalCRC = compute8Xor(data);
-				std::uint32_t noiseCRC = compute8Xor(noise);
+					// std::uint32_t originalCRC = compute8Xor(data);
+					// std::uint32_t noiseCRC = compute8Xor(noise);
 
-				if (std::strncmp((const char *)data.data(), (const char *)noise.data(),
-								 data.size() * sizeof(unsigned int)) != 0 &&
-					originalCRC == noiseCRC) {
-					nrCollision++;
+					if (std::strncmp((const char *)data.data(), (const char *)noise.data(),
+									 data.size() * sizeof(unsigned int)) != 0 &&
+						originalCRC == noiseCRC) {
+						nrCollision++;
+					}
 				}
-			}
-			// Decrement the WaitGroup counter when the task has finished.
+				// Decrement the WaitGroup counter when the task has finished.
 
-			// Blocking in a task?
-			// The scheduler will find something else for this thread to do.
-			uint64_t _current_nr_samples = nrOfSamples.fetch_add(localsamples);
+				// Blocking in a task?
+				// The scheduler will find something else for this thread to do.
+				uint64_t _current_nr_samples = nrOfSamples.fetch_add(localsamples);
 
-			const double _collisionPerc = (double)nrCollision.load() / (double)_current_nr_samples;
-			printf("CRC8 collision: %ld, %d, %f!\n", _current_nr_samples, nrCollision.load(), _collisionPerc);
-			defer(saidHello.done());
-			sayHello.wait();
+				const double _collisionPerc = (double)nrCollision.load() / (double)_current_nr_samples;
+				printf("\rCRC: %s, NumberOfSamples %ld, collision: [%d,%lf]", "CRC8", _current_nr_samples,
+					   nrCollision.load(), _collisionPerc);
+				fflush(stdout);
 
-		});
+				/*	*/
+				defer(saidHello.done());
+				sayHello.wait();
+
+			});
+		}
+
+		const double collisionPerc = (double)nrCollision.load() / (double)samples;
+
+		sayHello.signal(); // Unblock all the tasks.
+
+		saidHello.wait(); // Wait for all tasks to complete.
+
+		printf("\rCRC8 collision %d, %f!\n", nrCollision.load(), collisionPerc * 100);
+	} catch (const std::exception &ex) {
+		std::cerr << ex.what();
+		return EXIT_FAILURE;
 	}
-
-	double collisionPerc = (double)nrCollision.load() / (double)samples;
-
-	sayHello.signal(); // Unblock all the tasks.
-
-	saidHello.wait(); // Wait for all tasks to complete.
-
-	printf("CRC8 collision %d, %f!\n", nrCollision.load(), collisionPerc * 100);
 	return EXIT_SUCCESS;
 }
